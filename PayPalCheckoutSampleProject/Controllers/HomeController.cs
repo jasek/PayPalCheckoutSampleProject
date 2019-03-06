a03361c6a83cd7580c082239638301f8b8865db5
jasek <jasek2@gmail.com>
2019-03-06 17:17:05 +1100

    Add project files.

---
+++ PayPalCheckoutSampleProject/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using PayPalCheckoutSdk.Core;
+using PayPalCheckoutSdk.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PayPalCheckoutSampleProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var clientId = "";
+            var secret = "";
+
+            //Creating a sandbox environment
+            PayPalEnvironment environment = new SandboxEnvironment(clientId, secret);
+
+            // Creating a client for the environment
+            PayPalHttpClient client = new PayPalHttpClient(environment);
+            var request = new OrdersGetRequest("");
+            var task = client.Execute(request);
+            var response = task.Result;
+            var statusCode = response.StatusCode;
+
+            ViewBag.Message = response.StatusCode.ToString();
+
+            return View(statusCode);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PayPalCheckoutSampleProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PayPalCheckoutSampleProject.MvcApplication" Language="C#" %>
